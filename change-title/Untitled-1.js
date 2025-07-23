66bbba430a530b01eb8df7206aaf115e0eb565cf
GLN-net <foofera9@gmail.com>
2025-07-23 21:07:35 +0200

    Add initial HTML, CSS, and JavaScript files for drum game interface

---
+++ change-title/Untitled-1.js
@@ -0,0 +1,85 @@
+$(".button1").click(function(){
+ $("h1").slideUp();
+});
+
+
+$(".button2").click(function(){
+ $("h1").slideDown();
+});
+
+
+$(".button3").click(function(){
+ $("h1").animate({opacity:"0.5"});
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//$(document).keypress(function(event){
+ // $("h1").text(event.key);
+//});
+
+//$("h1").on("mouseover",function(){ [maouseover/click/dom events...]
+  //$("h1").css("color","red");
+//});
+
+//$("button").click(function(){
+ //$("h1").toggle();
+//});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
